d1e9e27197be8dc3be7cc75f9ba449ca13ead96b
wfiona <877936148@qq.com>
2021-03-09 10:27:31 +0800

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as fg
+from pygame.sprite import Group
+
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+
+    #开始游戏的主循环
+    while True:
+        #监视键盘和鼠标的事件
+        
+        fg.check_events(ai_settings,screen,ship,bullets)
+        
+        ship.update()
+
+        fg.update_bullets(bullets)
+            
+        fg.update_screen(ai_settings,screen,ship,bullets)
+
+
+
+
+
+
+
+
+run_game()
